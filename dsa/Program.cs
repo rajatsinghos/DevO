0d700ed4ed9b744079e9e2b096bf6696ca73d4ae
rajatsinghos <rajatsinghraj4321@gmail.com>
2026-09-20 18:02:21 +0530

    dsa added

---
+++ dsa/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.AccessControl;
+using System.Collections.Generic;
+using System.Linq;
+class program
+{
+   public static int LengthofLongestSubstring(string s)
+    {
+        HashSet<char> list = new HashSet<char>();
+
+        int start = 0;
+        int maxlengtH = 0;
+
+        for (int end = 0; end < s.Length; end++)
+        {
+            while (list.Contains(s[end]))
+            {
+                list.Remove(s[start]);
+                start++;
+            }
+            list.Add(s[end]);
+            maxlengtH = Math.Max(maxlengtH, end - start + 1);
+        }
+        return maxlengtH;       
+    }
+}
