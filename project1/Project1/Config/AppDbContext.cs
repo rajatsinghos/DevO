2ffd6d17104fd4a4b54a0e4a30133a87047fea2e
rajatsinghos <rajatsinghraj4321@gmail.com>
2026-09-20 20:56:29 +0530

    new files has been added

---
+++ project1/Project1/Config/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Project1.Config
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
